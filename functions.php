f5314e6e3b981e265e12fbdf549c40bbf83822dc
Grant Corley <grantcorley@gmail.com>
2018-12-02 16:30:32 -0500

    attempted to move nav bar above image, coudn't get consistent result

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ add_action( 'wp_enqueue_scripts','custom_scripts' );
 
 function custom_scripts(){
 		wp_enqueue_style('oswald', 'https://fonts.googleapis.com/css?family=Oswald');
+)
 
 } //end secdc_custom_scripts
 
